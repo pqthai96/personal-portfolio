ebffbf5a00f6e9cb9a946645ca159b34b038339c
pqthai96 <111183239+pqthai96@users.noreply.github.com>
2024-01-08 17:32:01 +0700

    header

---
+++ components/section-heading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export interface SectionHeadingProps {
+  children: React.ReactNode
+}
+function SectionHeading({children}: SectionHeadingProps) {
+  return (
+    <h2 className="text-3xl font-medium capitalize mb-8 text-center">{children}</h2>
+  );
+}
+
+export default SectionHeading;
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Course: https://www.youtube.com/watch?v=sUKptmUVIBM
+Git Repo: https://github.com/ByteGrad/portfolio-website/tree/master
