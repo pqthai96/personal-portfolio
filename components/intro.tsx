7a13e41072df743a2d8de0ceec9520b9a68b7a96
pqthai96 <111183239+pqthai96@users.noreply.github.com>
2024-01-10 15:26:55 +0700

    update name

--- components/intro.tsx
+++ components/intro.tsx
@@ -47,7 +47,7 @@ function Intro() {
         animate={{opacity: 1, y: 0}}
       >
         {/* eslint-disable-next-line react/no-unescaped-entities */}
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Thái Brother.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
